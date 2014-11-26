708f2145b7987291a9076a6d892d47a3a51c6954
Jakub Szalek <jszalek@sointeractive.pl>
2014-11-26 14:00:01 +0100

    ISAACLOUD-1322 templates generator

--- models/templatesManager.js
+++ models/templatesManager.js
@@ -1,8 +1,27 @@
-var fs = require('fs');
+var fs = require('fs'),
+    faker = require('faker');
+
+function prepareUrl(url) {
+    var newBase = url.split('/');
+    newBase.pop();
+    newBase = newBase.join('/');
+    return newBase + '/templates'
+}
 
 function readTemplates() {
-    fs.readdir()
+
+    var url = prepareUrl(lapi.argv.r);
+
+    fs.readdir(url, function (err, files) {
+        if (err) {
+            throw new Error(err);
+        }
+        console.log(files);
+        console.log(require('../' + url + '/' + files[0]));
+    })
 }
+
+readTemplates();
 
 module.exports = {
 
