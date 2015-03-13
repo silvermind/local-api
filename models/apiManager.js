40889f276af7ef1f4f45431fe61a8f176d106ac5
Jakub Szalek <jszalek@sointeractive.pl>
2015-03-13 14:45:12 +0100

    ISAACLOUD-1605 add support for custom headers in response

--- models/apiManager.js
+++ models/apiManager.js
@@ -1,194 +1,215 @@
 var _ = require('lodash'),
-    amanda = require('amanda'),
-    Q =  require('q'),
-    ramlRoot,
-    methodToValidate = ['post', 'put'];
+  amanda = require('amanda'),
+  Q =  require('q'),
+  ramlRoot,
+  methodToValidate = ['post', 'put'];
 
 var getResponse = function (ramlRoot, req){
 
-    var deffered = Q.defer();
+  var deffered = Q.defer();
 
-    var contentType, preparedPath, currentResource, currentMethod, successResponse, validationSchema, postPutReq;
+  var contentType, preparedPath, currentResource, currentMethod, successResponse, validationSchema, postPutReq,
+    successResponseObj, currentHeaders;
 
-    contentType = localUtils.getContentType(req);
+  contentType = localUtils.getContentType(req);
 
-    req.method = req.method.toLowerCase();
+  req.method = req.method.toLowerCase();
 
-    // prepare req path for searching
-    preparedPath = localUtils.pathPrepare(req.path);
+  // prepare req path for searching
+  preparedPath = localUtils.pathPrepare(req.path);
 
-    // find current resource in raml definitions
-    currentResource = localUtils.findResource(ramlRoot, preparedPath);
+  // find current resource in raml definitions
+  currentResource = localUtils.findResource(ramlRoot, preparedPath);
 
-    // find chosen method in raml definitions
-    currentMethod = localUtils.findMethod(currentResource, req.method);
+  // find chosen method in raml definitions
+  currentMethod = localUtils.findMethod(currentResource, req.method);
 
-    // find success response in this resource
-    successResponse = localUtils.findSuccessResponse(currentMethod.responses, contentType, req.method);
+  // find success response in this resource
+  successResponseObj = localUtils.findSuccessResponse(currentMethod.responses, contentType, req.method);
+  successResponse = successResponseObj.body;
 
-    // check if POST or PUT method
-    postPutReq = methodToValidate.indexOf(req.method) >= 0;
+  // find success headers in this resource
+  currentHeaders = successResponseObj.headers;
 
-    // find validation schema for request data
-    validationSchema = postPutReq ? localUtils.findValidationSchema(currentMethod, contentType) : null;
+  // check if POST or PUT method
+  postPutReq = methodToValidate.indexOf(req.method) >= 0;
 
-    // check if sent data is valid (POST, PUT)
-    if (validationSchema) {
+  // find validation schema for request data
+  validationSchema = postPutReq ? localUtils.findValidationSchema(currentMethod, contentType) : null;
 
-        localUtils.validateJson(req.body, validationSchema, function (err) {
-            var _finalRes;
-            if (err) {
-                _finalRes = {
-                    data: {
-                        message: err['0'].message
-                    },
-                    code: 400
-                }
-            } else {
+  // check if sent data is valid (POST, PUT)
+  if (validationSchema) {
 
-                var resBody;
+    localUtils.validateJson(req.body, validationSchema, function (err) {
+      var _finalRes;
+      if (err) {
+        _finalRes = {
+          data: {
+            message: err['0'].message
+          },
+          code: 400
+        }
+      } else {
 
-                var example = JSON.parse(successResponse.example);
+        var resBody;
 
-                if (_.isObject(req.body) && _.isObject(example)) {
-                    resBody = _.clone(example, true);
-                    for (var key in req.body) {
-                        resBody[key] = req.body[key];
-                    }
-                } else {
-                    resBody = req.body;
-                }
+        var example = JSON.parse(successResponse.example);
 
-                _finalRes = {
-                    data: resBody,
-                    code: 200
-                }
-            }
-            deffered.resolve(_finalRes);
-        });
+        if (_.isObject(req.body) && _.isObject(example)) {
+          resBody = _.clone(example, true);
+          for (var key in req.body) {
+            resBody[key] = req.body[key];
+          }
+        } else {
+          resBody = req.body;
+        }
 
-    } else {
-        // send response
-        deffered.resolve({
-            data: successResponse.example,
-            code: 200
-        });
-    }
+        _finalRes = {
+          data: resBody,
+          code: 200,
+          headers: currentHeaders
+        }
+      }
+      deffered.resolve(_finalRes);
+    });
 
-    return deffered.promise;
+  } else {
+    // send response
+    deffered.resolve({
+      data: successResponse.example,
+      code: 200,
+      headers: currentHeaders
+    });
+  }
+
+  return deffered.promise;
 
 };
 
 var localUtils = {
 
-    pathPrepare: function(path) {
-        var p = path.split('/');
-        p.splice(0,1);
+  pathPrepare: function(path) {
+    var p = path.split('/');
+    p.splice(0,1);
 
-        if (_.last(p) === '') {
-            p.pop();
-        }
-
-        return p;
-    },
-
-    getContentType: function (req) {
-        var contentType = req.header('Content-Type');
-        return contentType ? contentType.split(';')[0] : null;
-    },
-
-    findResource: function (ramlRoot, preparedPath) {
-        var currentResource = ramlRoot,
-            elementName, nextElement, relativeUri;
-
-        for (var i = 0, l=preparedPath.length; i < l; i++){
-
-            elementName = preparedPath[i];
-            nextElement = _.find(currentResource.resources, function(resource){
-                relativeUri = resource.relativeUri.substring(1);
-                return relativeUri === elementName;
-            });
-
-            if (!nextElement) {
-                nextElement = _.find(currentResource.resources, function(resource){
-                    relativeUri = resource.relativeUri.substring(1);
-                    return relativeUri.match(/{(.*?)}/);
-                });
-            }
-
-            if(nextElement) {
-                currentResource = nextElement;
-            } else {
-                throw new Error('Specified path not in raml');
-            }
-
-        }
-
-        return currentResource;
-    },
-
-    findMethod: function (resource, method) {
-        var res = _.find(resource.methods, {method: method});
-        if (res) {
-            return res;
-        } else {
-            throw new Error('Specified method not in raml');
-        }
-    },
-
-    findSuccessResponse: function (responses, contentType, method) {
-        var code = responses['200'] || responses['201'] || responses ['202'];
-        if (!code) {throw new Error('Success response is not specified fot this resource');}
-
-        var body = code.body;
-        if (!body) {throw new Error('Body is not specified fot this resource');}
-
-        var succ;
-        if (contentType) {
-            succ = body[contentType];
-            if (!succ) {throw new Error('Content-Type ' + contentType + ' is not specified fot this resource');}
-        } else {
-            succ = body['application/json'];
-            if (!succ) {throw new Error('No data for undefined Content-Type');}
-        }
-
-        return succ;
-    },
-
-    findValidationSchema: function (method, contentType) {
-        return method && method.body && method.body[contentType] && method.body[contentType].schema ? JSON.parse(method.body[contentType].schema) : null;
-    },
-
-    validateJson: function (body, schema, succ) {
-        var jsonSchemaValidator = amanda('json');
-        jsonSchemaValidator.validate(body, schema, succ);
+    if (_.last(p) === '') {
+      p.pop();
     }
+
+    return p;
+  },
+
+  getContentType: function (req) {
+    var contentType = req.header('Content-Type');
+    return contentType ? contentType.split(';')[0] : null;
+  },
+
+  findResource: function (ramlRoot, preparedPath) {
+    var currentResource = ramlRoot,
+      elementName, nextElement, relativeUri;
+
+    for (var i = 0, l=preparedPath.length; i < l; i++){
+
+      elementName = preparedPath[i];
+      nextElement = _.find(currentResource.resources, function(resource){
+        relativeUri = resource.relativeUri.substring(1);
+        return relativeUri === elementName;
+      });
+
+      if (!nextElement) {
+        nextElement = _.find(currentResource.resources, function(resource){
+          relativeUri = resource.relativeUri.substring(1);
+          return relativeUri.match(/{(.*?)}/);
+        });
+      }
+
+      if(nextElement) {
+        currentResource = nextElement;
+      } else {
+        throw new Error('Specified path not in raml');
+      }
+
+    }
+
+    return currentResource;
+  },
+
+  findMethod: function (resource, method) {
+    var res = _.find(resource.methods, {method: method});
+    if (res) {
+      return res;
+    } else {
+      throw new Error('Specified method not in raml');
+    }
+  },
+
+  findSuccessResponse: function (responses, contentType, method) {
+    var code = responses['200'] || responses['201'] || responses ['202'];
+    if (!code) {throw new Error('Success response is not specified fot this resource');}
+
+    var body = code.body,
+      headers = code.headers;
+    if (!body) {throw new Error('Body is not specified fot this resource');}
+
+    var succ;
+    if (contentType) {
+      succ = body[contentType];
+      if (!succ) {throw new Error('Content-Type ' + contentType + ' is not specified fot this resource');}
+    } else {
+      succ = body['application/json'];
+      if (!succ) {throw new Error('No data for undefined Content-Type');}
+    }
+
+    return {
+      body: succ,
+      headers: headers
+    };
+  },
+
+  findValidationSchema: function (method, contentType) {
+    return method && method.body && method.body[contentType] && method.body[contentType].schema ? JSON.parse(method.body[contentType].schema) : null;
+  },
+
+  validateJson: function (body, schema, succ) {
+    var jsonSchemaValidator = amanda('json');
+    jsonSchemaValidator.validate(body, schema, succ);
+  },
+
+  setCustomHeaders: function (headers, res) {
+    for (var key in headers) {
+      var curr = headers[key];
+      res.set(key, curr.example);
+    }
+  }
 
 }
 
 module.exports = {
 
-    setRamlRoot: function (raml) {
-        ramlRoot = raml;
-    },
+  setRamlRoot: function (raml) {
+    ramlRoot = raml;
+  },
 
-    ramlMethods: function(req,res){
+  ramlMethods: function(req,res){
 
-        var self = this;
+    var self = this;
 
-        try{
-            getResponse(ramlRoot, req).then(function (ramlRes) {
-                res.status(ramlRes.code).send(ramlRes.data);
-            });
-        } catch(e){
-            console.log(e);
-            res.status(404).send({
-                "message": "This resource does not exist, look into the documentation",
-                "code": 40402
-            });
+    try{
+      getResponse(ramlRoot, req).then(function (ramlRes) {
+        if (ramlRes.headers) {
+          localUtils.setCustomHeaders(ramlRes.headers, res);
         }
-
+        res.status(ramlRes.code).send(ramlRes.data);
+      });
+    } catch(e){
+      console.log(e);
+      res.status(404).send({
+        "message": "This resource does not exist, look into the documentation",
+        "code": 40402
+      });
     }
+
+  }
 
 
 
